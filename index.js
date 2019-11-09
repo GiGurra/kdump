5c050ab6f708bf6de9c090c8e228b89c909093c5
Johan Kjölhede <johan.kjolhede@softwarelse.se>
2019-11-09 15:07:02 +0100

    Support cmd line options, for filtering on contexts, namespaces, etc

--- index.js
+++ index.js
@@ -2,19 +2,28 @@
 
 const execSync = require('child_process').execSync;
 const fs = require("fs");
+const yargs = require("yargs");
 
 const tableParser = require('table-parser');
 
 async function main() {
 
+    const cmdLine = parseCmdLine();
+
     console.log("Running kube-dump script");
 
-    const contexts = getContexts();
+    const contexts = (cmdLine.c || getContexts()).filter( c => {
+        return !cmdLine.ec || cmdLine.ec.indexOf(c) < 0
+    });
+
     console.log("contexts: " + contexts);
 
     for (const context of contexts) {
         console.log(" - processing context '" + context + "'");
 
+        if (fs.existsSync(context)) {
+            throw new Error("Output directory '" + context + "' already exists!");
+        }
         fs.mkdirSync(context, {recursive: true});
 
         execSync("kubectl config use-context " + context);
@@ -23,12 +32,22 @@ async function main() {
             tableParser
                 .parse(execSync("kubectl api-resources -o wide").toString())
                 .map(toResource)
-                .filter(allowedResource);
+                .filter(isReadableResource)
+                .filter(r => !cmdLine.er || cmdLine.er.indexOf(r.name) < 0);
 
-        const namespacedResources = allResources.filter(r => r.namespaced);
-        const globalResources = allResources.filter(r => !r.namespaced);
+        const namespacedResources =
+            allResources
+                .filter(r => r.namespaced)
+                .filter(r => !cmdLine.nr || cmdLine.nr.indexOf(r.name) >= 0);
 
-        const namespaces = getNamespaces();
+        const globalResources =
+            allResources
+                .filter(r => !r.namespaced)
+                .filter(r => !cmdLine.gr || cmdLine.gr.indexOf(r.name) >= 0);
+
+        const namespaces = (cmdLine.n || getNamespaces()).filter(n => {
+            return !cmdLine.en || cmdLine.en.indexOf(n) < 0
+        });
 
         for (const namespace of namespaces) {
 
@@ -47,20 +66,74 @@ async function main() {
 
         }
 
-        console.log("   - processing global resources for context");
-
-        for (const globalResource of globalResources) {
-
-            const resourceYaml = execSync("kubectl get " + globalResource.name + " -o yaml").toString();
-            fs.writeFileSync(context + "/" + globalResource.name + ".yml", resourceYaml)
+        if (cmdLine.eg) {
+            console.log("   - NOT processing global resources for context, since --eg flag was specified");
         }
+        else {
 
+            console.log("   - processing global resources for context");
+
+            for (const globalResource of globalResources) {
+
+                const resourceYaml = execSync("kubectl get " + globalResource.name + " -o yaml").toString();
+                fs.writeFileSync(context + "/" + globalResource.name + ".yml", resourceYaml)
+            }
+
+        }
     }
 
     console.log("kube-dump script finished!")
 }
 
-function allowedResource(resource) {
+function parseCmdLine() {
+    return yargs(process.argv.slice(2))
+        .option('context', {
+            alias: 'c',
+            description: 'Specify contexts. If omitted - use all available',
+            type: 'array'
+        })
+        .option('exclude-context', {
+            alias: 'ec',
+            description: 'Exclude contexts',
+            type: 'array'
+        })
+        .option('namespace', {
+            alias: 'n',
+            description: 'Specify namespaces. If omitted - use all available',
+            type: 'array',
+        })
+        .option('exclude-namespace', {
+            alias: 'en',
+            description: 'Exclude namespaces',
+            type: 'array',
+        })
+        .option('exclude-global', {
+            alias: 'eg',
+            description: 'Exclude global (non-namespaced) data',
+            type: 'boolean',
+            default: false
+        })
+        .option('namespaced-resource', {
+            alias: 'nr',
+            description: 'Specify namespaced resources. If omitted - use all available',
+            type: 'array'
+        })
+        .option('global-resource', {
+            alias: 'gr',
+            description: 'Specify global resources. If omitted - use all available',
+            type: 'array'
+        })
+        .option('exclude-resource', {
+            alias: 'er',
+            description: 'Exclude resource',
+            type: 'array'
+        })
+        .help()
+        .strict()
+        .argv;
+}
+
+function isReadableResource(resource) {
     return resource.verbs.indexOf('get') >= 0;
 }
 
