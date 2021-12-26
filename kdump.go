dc47f297a371e023678cea2f7c63f124553db734
Johan Kjölhede <gigurra@gmail.com>
2021-12-26 16:46:14 +0100

    wip

--- kdump.go
+++ kdump.go
@@ -25,10 +25,15 @@ func main() {
 func dumpCurrentContext(outputDir string, allowOverwrite bool) {
 
 	if allowOverwrite {
-		os.RemoveAll(outputDir)
+		_ = os.RemoveAll(outputDir)
 	}
 
 	fileutil.PanicIfExists(outputDir, fmt.Sprintf("output folder '%s' already exists!", outputDir), fmt.Sprintf("output folder '%s' inaccessible!", outputDir))
 	fileutil.CreateFolderOrPanic(outputDir, fmt.Sprintf("could not create folder '%s'", outputDir))
+
 	log.Printf("Downloading all resources from current context to dir %s ...\n", outputDir)
+
+	namespaces := kubectl.Namespaces()
+	log.Printf("Namespaces: %v ...\n", namespaces)
+
 }
