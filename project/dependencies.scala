037ee20428a1c7b1a071acbe67f9a919018126ef
Johan Kjölhede <gigurra@gmail.com>
2021-12-27 10:14:43 +0100

    wip on scala version

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -3,11 +3,13 @@ import sbt._
 object dependencies {
 
   object circe extends scalaLibSource("io.circe", "0.14.1")
+  object osLib extends scalaLibSource("com.lihaoyi", "0.8.0")
 
   val prod = Seq(
     circe("circe-yaml"),
     circe("circe-parser"),
     circe("circe-generic"),
+    osLib("os-lib"),
   )
 }
 
--- src/main/scala/com/github/gigurra/kdump/internal/util/file.scala
+++ src/main/scala/com/github/gigurra/kdump/internal/util/file.scala
@@ -3,20 +3,20 @@ package com.github.gigurra.kdump.internal.util
 import java.io.File
 import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Paths}
-import com.github.gigurra.kdump.internal.util.JavaDeleteFolder
 
 object file {
 
-  def delete(path: String): Unit = JavaDeleteFolder.deleteDirectoryStream(Paths.get(path))
+  def resolve(path: String): os.Path = os.Path(Paths.get(path).toAbsolutePath)
 
-  def exists(path: String): Boolean = Files.exists(Paths.get(path))
+  def delete(path: String): Unit = os.remove.all(resolve(path))
 
-  def mkDirs(path: String): Unit = Files.createDirectories(Paths.get(path))
+  def exists(path: String): Boolean = os.exists(resolve(path))
 
-  def string2File(path: String, contents: String): Unit = Files.writeString(Paths.get(path), contents, StandardCharsets.UTF_8)
+  def mkDirs(path: String): Unit = os.makeDir.all(resolve(path))
+
+  def string2File(path: String, contents: String): Unit = os.write.over(resolve(path), contents)
 
   def sanitizeFileName(name: String): String = "[^a-zA-Z0-9\\-_.]+".r.replaceAllIn(name, "_")
 
-  def listFilesInDir(dir: String): List[File] =
-    Option(new File(dir).listFiles()).toList.flatMap(_.toList)
+  def listFilesInDir(dir: String): Seq[File] = if exists(dir) then os.list(resolve(dir)).map(_.toIO) else Nil
 }
