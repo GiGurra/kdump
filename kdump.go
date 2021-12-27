b507a5991deea91612b5afaa0599f3bc04d4c9fd
Johan Kjölhede <gigurra@gmail.com>
2021-12-27 23:29:15 +0100

    wip

--- kdump.go
+++ kdump.go
@@ -18,7 +18,7 @@ func dumpCurrentContext(appConfig config.AppConfig) {
 
 	log.Printf("Downloading all resources from current context")
 
-	outputDir := appConfig.OutputDir
+	outputDirRoot := appConfig.OutputDir
 	namespaces := kubectl.NamespacesOrPanic()
 	apiResourceTypes := kubectl.ApiResourceTypesOrPanic()
 	resourcesToDownload := appConfig.FilterIncludeResources(apiResourceTypes.Accessible.All)
@@ -28,15 +28,15 @@ func dumpCurrentContext(appConfig config.AppConfig) {
 
 	parsed := kubectl.ParseK8sYamlOrPanic(everything)
 
-	log.Printf("Deleting old data in '%s'...\n", outputDir)
+	log.Printf("Deleting old data in '%s'...\n", outputDirRoot)
 
-	fileutil.PanicIfCantDelete(outputDir, fmt.Sprintf("removal of outputdir '%s' failed", outputDir))
-	fileutil.PanicIfExists(outputDir, fmt.Sprintf("output folder '%s' already exists!", outputDir), fmt.Sprintf("output folder '%s' inaccessible!", outputDir))
-	fileutil.CreateFolderOrPanic(outputDir, fmt.Sprintf("could not create folder '%s'", outputDir))
+	fileutil.PanicIfCantDelete(outputDirRoot, fmt.Sprintf("removal of outputdir '%s' failed", outputDirRoot))
+	fileutil.PanicIfExists(outputDirRoot, fmt.Sprintf("output folder '%s' already exists!", outputDirRoot), fmt.Sprintf("output folder '%s' inaccessible!", outputDirRoot))
+	fileutil.CreateFolderOrPanic(outputDirRoot, fmt.Sprintf("could not create folder '%s'", outputDirRoot))
 
-	log.Printf("Storing resources in '%s'...\n", outputDir)
+	log.Printf("Storing resources in '%s'...\n", outputDirRoot)
 	for _, ns := range namespaces {
-		nsOutputDir := outputDir + "/" + ns
+		nsOutputDir := outputDirRoot + "/" + ns
 		fileutil.CreateFolderOrPanic(nsOutputDir, "could not create folder: "+nsOutputDir)
 	}
 
@@ -45,10 +45,10 @@ func dumpCurrentContext(appConfig config.AppConfig) {
 		if resource.IsSecret() {
 			log.Printf("Ignoring secret storage (not yet implemented) for %s/%s: ", resource.MetaData.Namespace, resource.MetaData.Name)
 		} else if resource.IsNamespaced() {
-			nsOutputDir := outputDir + "/" + resource.MetaData.Namespace
+			nsOutputDir := outputDirRoot + "/" + resource.MetaData.Namespace
 			fileutil.String2FileOrPanic(nsOutputDir+"/"+filename, resource.SourceYaml)
 		} else {
-			fileutil.String2FileOrPanic(outputDir+"/"+filename, resource.SourceYaml)
+			fileutil.String2FileOrPanic(outputDirRoot+"/"+filename, resource.SourceYaml)
 		}
 	}
 }
