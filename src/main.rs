9296b425715a92d0b0cff1be8c48ffe6ddda64ba
Johan Kjölhede <gigurra@gmail.com>
2021-12-29 21:16:20 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
 mod util;
 
-fn main() {
-    let output_dir = "test";
+struct AppConfig {
+    output_dir: String,
+    delete_prev_dir: bool,
+}
 
-    print!("Checking if output dir '{}' exists... ", output_dir);
+impl Default for AppConfig {
+    fn default() -> Self {
+        return AppConfig {
+            output_dir: String::from("test"),
+            delete_prev_dir: false
+        };
+    }
+}
+
+fn main() {
+    println!("Checking output dir..");
+    let appConfig = AppConfig::default();
+    let root_output_dir = appConfig.output_dir;
+
+    print!("Checking if output dir '{}' exists... ", root_output_dir);
 
     let output_dir_already_exists = util::file::path_exists("test");
 
@@ -20,4 +36,25 @@ fn main() {
         let err_str = std::str::from_utf8(&command_output.stderr).unwrap();
 
         println!("Hello, world!, cmd line result={}, output={}, err={}", status_code, output_str, err_str);*/
+}
+
+fn ensure_root_output_dir(appConfig: AppConfig) {
+
+    if appConfig.delete_prev_dir {
+        util::file::delete_all_if_exists(&appConfig.output_dir)
+    }
+    /*
+
+        if appConfig.DeletePrevDir {
+            fileutil.DeleteIfExists(out, fmt.Sprintf("removal of outputdir '%s' failed", out))
+        }
+
+        if fileutil.Exists(out, fmt.Sprintf("checking outputdir '%s' failed", out)) {
+            log.Fatal("Bailing! output-dir already exists: " + out)
+        }
+
+        fileutil.CreateFolderIfNotExists(out, fmt.Sprintf("could not create folder '%s'", out))
+
+        return out*/
+
 }
--- src/util/file/mod.rs
+++ src/util/file/mod.rs
@@ -3,3 +3,9 @@ use std::path::Path;
 pub fn path_exists(path: &str) -> bool {
     return Path::new(path).exists();
 }
+
+pub fn delete_all_if_exists(path: &str) {
+    if path_exists(path) {
+        std::fs::remove_dir_all(Path::new(path)).unwrap();
+    }
+}
