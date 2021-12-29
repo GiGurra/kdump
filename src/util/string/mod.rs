c1964b2be969f52a545e4e263f81cd8327428753
Johan Kjölhede <gigurra@gmail.com>
2021-12-29 23:06:12 +0100

    wip

--- src/util/string/mod.rs
+++ src/util/string/mod.rs
@@ -1,20 +1,37 @@
+use std::fmt::{Debug, Formatter};
+
+#[derive(Debug)]
+pub struct StdOutTableColumn {
+    name: String,
+    byte_offset: usize,
+}
 
 pub fn parse_stdout_table(lines: &Vec<String>) {
-
     let headings_line = &lines[0]; // .split_whitespace().collect::<Vec<&str>>()
     println!("head: {:?}", headings_line);
 
-    let mut begin_indices: Vec<usize> = Vec::<usize>::new();
+    let mut begin_offsets = Vec::<usize>::new();
     let mut prev_is_space = true;
 
-    for (i, c) in headings_line.chars().enumerate() {
-
-        if prev_is_space && !c.is_whitespace() {
-            begin_indices.push(i);
+    for (i, c) in headings_line.bytes().enumerate() {
+        if prev_is_space && !c.is_ascii_whitespace() {
+            begin_offsets.push(i);
         }
-        prev_is_space = c.is_whitespace();
-
+        prev_is_space = c.is_ascii_whitespace();
     }
 
-    println!("beginIndices: {:?}", begin_indices);
+    let mut headings = Vec::<StdOutTableColumn>::new();
+    for (vec_index, byte_offset) in begin_offsets.iter().enumerate() {
+        let end_offset: usize =
+            if vec_index + 1 < begin_offsets.len() {
+                begin_offsets[vec_index + 1]
+            } else {
+                headings_line.len()
+            };
+        let name_bytes = &headings_line.as_bytes()[*byte_offset..end_offset];
+        let name = String::from(String::from_utf8(Vec::from(name_bytes)).unwrap().trim());
+
+        headings.push(StdOutTableColumn { name, byte_offset: *byte_offset });
+    }
+    println!("headings: {:?}", headings);
 }
