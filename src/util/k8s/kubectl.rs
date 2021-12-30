78e379feecc654ddd0d64d20b3e22a4a6233abf2
Johan Kjölhede <gigurra@gmail.com>
2021-12-30 13:07:15 +0100

    wip

--- src/util/k8s/kubectl.rs
+++ src/util/k8s/kubectl.rs
@@ -1,7 +1,8 @@
 use std::str::FromStr;
 use regex::Regex;
-use crate::util::k8s::{ApiResourceType, ApiVersion};
-use super::super::super::*; // access all modules between util modules
+use std::collections::HashMap;
+use crate::util::k8s::*;
+use crate::util;
 
 pub fn api_resource_types() -> Vec<super::ApiResourceType> {
     let result = util::shell::run_command(
@@ -18,28 +19,12 @@ pub fn api_resource_types() -> Vec<super::ApiResourceType> {
 }
 
 fn map_to_resource_type(map: &HashMap<String, String>) -> ApiResourceType {
-    let mut result = ApiResourceType {
+    return ApiResourceType {
         name: String::from(&map["NAME"]),
         short_names: util::string::split_to_vec(&map["SHORTNAMES"], ",", true),
         namespaced: bool::from_str(&map["NAMESPACED"]).unwrap(),
         kind: String::from(&map["KIND"]),
         verbs: util::string::split_to_vec_r(util::string::remove_wrap(&map["VERBS"]), &Regex::new(r"\s+").unwrap(), true),
-        api_version: ApiVersion {
-            name: "".to_string(),
-            version: "".to_string(),
-        },
-        qualified_name: "".to_string(),
+        api_version: parse_api_version(&map["APIVERSION"]),
     };
-
-    let api_version_str_parts = util::string::split_to_vec(&map["APIVERSION"], "/", true);
-    if api_version_str_parts.len() > 1 {
-        result.api_version.name = api_version_str_parts[0].to_string();
-        result.api_version.version = api_version_str_parts[1].to_string();
-        result.qualified_name = result.name.clone() + "." + &result.api_version.name.clone();
-    } else {
-        result.api_version.version = api_version_str_parts[0].to_string();
-        result.qualified_name = result.name.clone();
-    };
-
-    return result;
 }
--- src/util/k8s/mod.rs
+++ src/util/k8s/mod.rs
@@ -1,5 +1,7 @@
 pub mod kubectl;
 
+use crate::util; // access all modules between util modules
+
 #[derive(Debug, PartialEq)]
 pub struct ApiVersion {
     name: String,
@@ -14,5 +16,29 @@ pub struct ApiResourceType {
     kind: String,
     verbs: Vec<String>,
     api_version: ApiVersion,
-    qualified_name: String,
+}
+
+impl ApiResourceType {
+    pub fn qualified_name(&self) -> String {
+        return if self.api_version.name.is_empty() {
+            self.name.clone()
+        } else {
+            self.name.clone() + "." + &self.api_version.name.clone()
+        };
+    }
+}
+
+pub fn parse_api_version(input: &str) -> ApiVersion {
+    let api_version_str_parts = util::string::split_to_vec(input, "/", true);
+    return if api_version_str_parts.len() > 1 {
+        ApiVersion {
+            name: api_version_str_parts[0].to_string(),
+            version: api_version_str_parts[1].to_string(),
+        }
+    } else {
+        ApiVersion {
+            name: "".to_string(),
+            version: api_version_str_parts[0].to_string(),
+        }
+    };
 }
