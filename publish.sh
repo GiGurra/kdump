f778e71fe70e36bdbd295bd34a91315b1448ec70
Johan Kjölhede <johan.kjolhede@rocker.com>
2023-02-16 20:44:33 +0100

    wip

--- publish.sh
+++ publish.sh
@@ -5,11 +5,10 @@ set -e
 source build.sh
 export DOCKER_TAG=gigurra/kdump:$VERSION
 
+echo "Publishing kdump $VERSION"
+
 git tag "$VERSION"
 git push origin "$VERSION"
 
 docker build . -t "$DOCKER_TAG"  --build-arg VERSION="$VERSION"
-
-echo "Publishing kdump $VERSION"
-
 docker push "$DOCKER_TAG"
